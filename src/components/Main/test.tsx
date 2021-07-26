661a24de384011e7111fbd20e194d1264971a4f8
Junior Santos <jnrplay@hotmail.com>
2021-07-26 15:52:17 -0300

    Add RTL config first test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Main from '.';
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Main />);
+    expect(screen.getByRole('heading', { name: /react avançado/i }));
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <main>
+        <h1>
+          React Avançado
+        </h1>
+      </main>
+    `);
+  });
+});
